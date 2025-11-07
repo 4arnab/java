58277b0c22849d93b8143e5673929046dc7beff3
4arnab <axmedarnab3@gmail.com>
2025-11-07 06:00:50 +0530

    accepting user input from the terminal using scanner class

--- Hello.java
+++ Hello.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] arg) {
-        System.out.println("Hello, world! FIRST TIME FOR EVERYTHING!");
+        Scanner scanner = new Scanner(System.in);
+
+        double width = 0;
+        double hight = 0;
+        double area = 0;
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the hight: ");
+        hight = scanner.nextDouble();
+
+        area = width * hight;
+        System.out.println("The Area is " + area + "cm^2");
+
+        scanner.close();
+        // System.out.println("Hello, world! FIRST TIME FOR EVERYTHING!");
     }
 }
